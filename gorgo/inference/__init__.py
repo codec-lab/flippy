6629bc5e6d377c60a9dc802c81f59c89873a8de3
Carlos Correa <cgcorrea@princeton.edu>
2022-12-16 11:58:05 -0500

    Add unit tests for inference algorithms.

--- gorgo/inference/__init__.py
+++ gorgo/inference/__init__.py
@@ -1,3 +1,8 @@
 from gorgo.inference.enumeration import Enumeration
 from gorgo.inference.sample_prior import SamplePrior
 from gorgo.inference.likelihood_weighting import LikelihoodWeighting
+from gorgo.core import Multinomial
+
+def _distribution_from_inference(dist):
+    ele, probs = zip(*dist.items())
+    return Multinomial(ele, probabilities=probs)
