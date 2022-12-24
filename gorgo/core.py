e9ef8e8629e9a543dd16ecc1b7d7903c80e8c067
markkho <markkho32@gmail.com>
2022-12-24 10:37:14 -0500

    Add distributions

--- gorgo/core.py
+++ gorgo/core.py
@@ -73,6 +73,134 @@ class Multinomial(StochasticPrimitive):
             for s in self.support
         })
 
+class ClosedInterval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+    def __contains__(self, ele):
+        return self.start <= ele <= self.end
+
+class IntegerInterval(ClosedInterval):
+    def __contains__(self, ele):
+        if ele == int(ele):
+            return self.start <= ele <= self.end
+        return False
+
+def beta_function(a, b):
+    num = math.gamma(a)*math.gamma(b)
+    den = math.gamma(a + b)
+    return num/den
+    
+class Gaussian(StochasticPrimitive):
+    support = ClosedInterval(float('-inf'), float('inf'))
+    def __init__(self, mean=0, sd=1):
+        self.mean = mean
+        self.sd = sd 
+        
+    def __call__(self, rng=random):
+        return rng.gauss(self.mean, self.sd)
+    
+    def log_probability(self, element):
+        prob = (
+            math.e**(-.5*((element - self.mean)/self.sd)**2)
+        )/(
+            self.sd*(2*math.pi)**.5
+        )
+        return math.log(prob) if prob > 0. else float('-inf')
+
+class Uniform(StochasticPrimitive):
+    def __init__(self, start=0, end=1):
+        self.start = start
+        self.end = end
+        self.support = ClosedInterval(start, end)
+        
+    def __call__(self, rng=random):
+        return rng.uniform(self.start, self.end)
+    
+    def log_probability(self, element):
+        if element in self.support:
+            return math.log(1/(self.end - self.start))
+        return float('-inf')
+    
+class Beta(StochasticPrimitive):
+    support = ClosedInterval(0, 1)
+    def __init__(self, alpha=1, beta=1):
+        self.a = alpha
+        self.b = beta
+        
+    def __call__(self, rng=random):
+        return rng.betavariate(self.a, self.b)
+    
+    def log_probability(self, element):
+        if element in self.support:
+            num = (element**(self.a - 1))*(1 - element)**(self.b - 1)
+            return math.log(num/beta_function(self.a, self.b))
+        return float('-inf')
+
+class Binomial(StochasticPrimitive):
+    def __init__(self, n : int, p : float):
+        self.n = n
+        self.p = p
+        assert 0 <= p <= 1
+        self.support = tuple(range(0, self.n + 1))
+        
+    def __call__(self, rng=random):
+        return sum(rng.random() < self.p for _ in range(self.n))
+    
+    def log_probability(self, element):
+        if element in self.support:
+            prob = (
+                math.comb(self.n, element)
+            )*(
+                self.p**element
+            )*(
+                (1 - self.p)**(self.n - element)
+            )
+            return math.log(prob)
+        return float('-inf')
+    
+class Geometric(StochasticPrimitive):
+    support = IntegerInterval(0, float('inf'))
+    def __init__(self, p : float):
+        self.p = p
+        assert 0 <= p <= 1
+        
+    def __call__(self, rng=random):
+        i = 0
+        while rng.random() >= self.p:
+            i += 1
+        return i
+    
+    def log_probability(self, element):
+        if element in self.support:
+            return math.log(
+                ((1 - self.p)**(element))*self.p
+            )
+        return float('-inf')
+    
+class BetaBinomial(StochasticPrimitive):
+    def __init__(self, n : int, alpha=1, beta=1):
+        self.a = alpha
+        self.b = beta
+        self.n = n
+        self.support = tuple(range(0, self.n + 1))
+        
+    def __call__(self, rng=random):
+        p = rng.betavariate(self.a, self.b)
+        return sum(rng.random() < p for _ in range(self.n))
+    
+    def log_probability(self, element):
+        if element in self.support:
+            prob = math.comb(self.n, element)*(
+                (
+                    beta_function(element+self.a, self.n - element + self.b)
+                )/(
+                    beta_function(self.a, self.b)
+                )
+            )
+            return math.log(prob)
+        return float('-inf')
+
 # TODO: finalize this interface
 class ObservationStatement:
     def __call__(
