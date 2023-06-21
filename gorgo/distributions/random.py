4fc962c859bb25121a79c38c86da2cc566bcffd3
markkho <markkho32@gmail.com>
2023-06-21 10:51:53 -0400

    Add explicit max 64 bit integer for randomness

--- gorgo/distributions/random.py
+++ gorgo/distributions/random.py
@@ -1,18 +1,20 @@
 import random
 
+MAX_INT64 = 2**64 - 1
+
 class BaseRandomNumberGenerator(random.Random):
     def fork(self, n : int):
-        return [self.__class__(self.randint(0, int(1e100))) for _ in range(n)]
+        return [self.__class__(self.randint(0, MAX_INT64)) for _ in range(n)]
 
     def new_seed(self):
-        return self.randint(0, int(1e100))
+        return self.randint(0, MAX_INT64)
 
 try:
     import numpy as np
     class RandomNumberGenerator(BaseRandomNumberGenerator):
-        def __init__(self, x=None):
-            super().__init__(x)
-            self.np = np.random.Generator(np.random.PCG64(self.randint(0, 2**32)))
+        def __init__(self, seed=None):
+            super().__init__(seed)
+            self.np = np.random.Generator(np.random.PCG64(self.randint(0, MAX_INT64)))
 except ImportError:
     RandomNumberGenerator = BaseRandomNumberGenerator
 
