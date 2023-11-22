ebfb8ffc3c6eff3482ed6df6adedd098974135ce
markkho <markkho@users.noreply.github.com>
2023-11-22 10:24:34 -0500

    Add support for stats in enumeration

--- gorgo/inference/enumeration.py
+++ gorgo/inference/enumeration.py
@@ -2,7 +2,7 @@ import heapq
 import math
 from collections import defaultdict
 import dataclasses
-from typing import Any, Union, List
+from typing import Any, Union, List, Tuple
 
 from gorgo.core import ProgramState, ReturnState, SampleState, ObserveState, InitialState
 from gorgo.interpreter import CPSInterpreter
@@ -14,10 +14,15 @@ class PrioritizedItem:
     priority: int
     item: Any=dataclasses.field(compare=False)
 
+@dataclasses.dataclass
+class EnumerationStats:
+    executions: int = 0
+
 class Enumeration:
     def __init__(self, function, max_executions=float('inf')):
         self.function = function
         self.max_executions = max_executions
+        self._stats = None
 
     def enumerate_tree(
         self,
@@ -55,6 +60,8 @@ class Enumeration:
                 executions += 1
             else:
                 raise ValueError("Unrecognized program state message")
+        if self._stats is not None:
+            self._stats.executions += executions
         return return_scores
 
     def run(self, *args, **kws):
@@ -62,3 +69,9 @@ class Enumeration:
         ps = init_ps.step(*args, **kws)
         return_scores = self.enumerate_tree(ps, self.max_executions)
         return Categorical.from_dict(softmax_dict(return_scores))
+
+    def _run_with_stats(self, *args, **kws) -> Tuple[Categorical, EnumerationStats]:
+        self._stats = EnumerationStats()
+        result = self.run(*args, **kws)
+        self._stats, stats = None, self._stats
+        return result, stats
